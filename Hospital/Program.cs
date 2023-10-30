758916678cb3aed2a39235be379ef06ea2538c87
karimWasea <131551998+karimWasea@users.noreply.github.com>
2023-10-30 16:56:01 +0200

    ff

--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -27,7 +27,7 @@ IServiceCollection serviceCollection = builder.Services.AddDbContext<Application
 
 
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount =  false)
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount =  true)
 .AddEntityFrameworkStores<ApplicationDBcontext>().AddDefaultTokenProviders();
 
 
