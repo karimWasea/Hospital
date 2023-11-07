71943b8cc99bcc5e42179d2edeaadc6ee41f8f1a
karimWasea <131551998+karimWasea@users.noreply.github.com>
2023-11-07 16:24:52 +0200

    dddddddddd

---
+++ Hospital/wwwroot/js/TimeSiftingSave.js
@@ -0,0 +1,39 @@
+﻿
+    document.addEventListener("DOMContentLoaded", function () {
+                                var saveForm = document.getElementById("saveform");
+
+    saveForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+    Swal.fire({
+        title: "Are you sure?",
+    text: "You are about to update the shift information.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Yes, update it!",
+    cancelButtonText: "No, cancel",
+                                }).then((result) => {
+                                if (result.isConfirmed) {
+        saveForm.submit();
+                                }
+                                }); 
+                                });
+                                });
+
+    document.addEventListener('DOMContentLoaded', function () {
+                                var startShiftInput = document.querySelector('#startshift');
+    var endsifitInput = document.querySelector('#Endsifit');
+    var durationInput = document.querySelector('#durationInput');
+
+    function updateDuration() {
+var startShiftValue = new Date(startShiftInput.value);
+    var endsifitValue = new Date(endsifitInput.value);
+    var durationMilliseconds = endsifitValue - startShiftValue;
+    var durationHours = durationMilliseconds / (1000 * 60 * 60);
+    durationInput.value = durationHours.toFixed(2);
+}
+
+    startShiftInput.addEventListener('change', updateDuration);
+    endsifitInput.addEventListener('change', updateDuration);
+});
+
---
+++ Hospital/wwwroot/js/dayweekworksaveget.js
@@ -0,0 +1,40 @@
+﻿
+
+    function saveData() {
+        const rows = document.querySelectorAll('tr');
+    const data = [];
+
+        rows.forEach(row => {
+            const doctorId = row.querySelector('input[type="hidden"]').value;
+    const ischekedday = row.querySelector('input#ischekedday').checked;
+    const isam = row.querySelector('input#isam').checked;
+    const ispm = row.querySelector('input#ispm').checked;
+
+    data.push({
+        DoctorId: doctorId,
+    ischekedday: ischekedday,
+    isam: isam,
+    ispm: ispm
+            });
+        });
+
+    const formData = JSON.stringify(data);
+
+    // Send data using AJAX
+    fetch('/Doctor/Daywork/Save', {
+        method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+            },
+    body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+        console.log('Response from server:', data);
+    alert('Data saved successfully!');
+            })
+            .catch(error => {
+        console.error('Error:', error);
+    alert('An error occurred while saving data.');
+            });
+    }
