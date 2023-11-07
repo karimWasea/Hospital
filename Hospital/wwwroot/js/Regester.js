720c1083562b1d5324664766acad5c86c9fb5154
karimWasea <131551998+karimWasea@users.noreply.github.com>
2023-11-07 16:27:48 +0200

    finshed

---
+++ Hospital/wwwroot/js/Regester.js
@@ -0,0 +1,37 @@
+﻿
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var saveForm = document.getElementById("registerForm"); // Change 'saveform' to 'registerForm'
+        var saveButton = document.getElementById("savedata");
+
+        saveForm.addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You are about to update the patient's information.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonText: "Yes, update it!",
+                cancelButtonText: "No, cancel",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // You can also add additional form validation here
+                    saveForm.submit();
+                }
+            });
+        });
+
+        // After form submission, show a success message if desired
+        var urlParams = new URLSearchParams(window.location.search);
+        var successParam = urlParams.get("success");
+        if (successParam === "true") {
+            Swal.fire({
+                title: "Success!",
+                text: "Patient's information has been updated.",
+                icon: "success"
+            });
+        }
+    });
+
---
+++ Hospital/wwwroot/js/patientregester.js
@@ -0,0 +1,36 @@
+﻿
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var saveForm = document.getElementById("registerForm");
+
+    saveForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+    Swal.fire({
+        title: "Are you sure?",
+    text: "You are about to update the patient's information.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Yes, update it!",
+    cancelButtonText: "No, cancel",
+            }).then((result) => {
+                if (result.isConfirmed) {
+        // You can add AJAX submission or simply let the form submit normally
+        saveForm.submit();
+                }
+            });
+        });
+
+    var urlParams = new URLSearchParams(window.location.search);
+    var successParam = urlParams.get("success");
+    if (successParam === "true") {
+        Swal.fire({
+            title: "Success!",
+            text: "Patient's information has been updated.",
+            icon: "success"
+        });
+        }
+    });
+
+
